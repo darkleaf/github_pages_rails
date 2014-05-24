75f6cdd2faed8ea32ba9a1641ee17671f166fffd
Mikhail Dronov <dronovmm@undev.ru>
2014-05-24 14:39:41 +0400

    removed some files

--- app/controllers/github_pages_rails/pages_controller.rb
+++ app/controllers/github_pages_rails/pages_controller.rb
@@ -2,9 +2,8 @@ require_dependency "github_pages_rails/application_controller"
 
 module GithubPagesRails
   class PagesController < ApplicationController
-
     def show
+      head :ok
     end
-
   end
 end
--- test/controllers/github_pages_rails/pages_controller_test.rb
+++ test/controllers/github_pages_rails/pages_controller_test.rb
@@ -2,11 +2,9 @@ require 'test_helper'
 
 module GithubPagesRails
   class PagesControllerTest < ActionController::TestCase
-
-    test "should show page" do
-      get :show, id: "sm_page", use_route: :github_pages_rails
+    test "get show" do
+      get :show, id: 'some-page', use_route: :github_pages_rails
       assert_response :success
     end
-
   end
 end
